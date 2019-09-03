ef9dd2b366f3ea288431cdbb8616db8892b20705
Unknown <chelle.pierre@gmail.com>
2019-09-03 17:48:18 -0400

    Methods to feed ggplot groupings from pk ratio mapping

--- R/pkratio-datamapping.R
+++ R/pkratio-datamapping.R
@@ -5,11 +5,26 @@
 PKRatioDataMapping <- R6::R6Class("PKRatioDataMapping",
   inherit = XYDataMapping,
   public = list(
-    grouping = NULL,
+    colorGrouping = NULL,
+    sizeGrouping = NULL,
+    shapeGrouping = NULL,
+      
     # Example of how to do some other stuff
-    initialize = function(x, y, grouping) {
-      self$grouping <-grouping;
+    initialize = function(x = "Age", y="Ratio", colorGrouping=NULL, sizeGrouping = NULL, shapeGrouping = NULL) {
+      self$colorGrouping <- colorGrouping
+      self$sizeGrouping <- sizeGrouping
+      self$shapeGrouping <- shapeGrouping
       super$initialize(x, y)
     }
   )
 )
+
+
+if (FALSE){
+  # Example of definition of Data Mapping
+  CmaxRatio.DataMapping <- list(
+    "Axes" = list("X"="Age", "Y"=c("Ratio")),
+    "Unit" = list("X"="yrs", "Y"=c(NULL)),
+    "Grouping" = list("Color"= "Gender", "Symbol"=c("Compound", "Dose"))
+  )
+}
