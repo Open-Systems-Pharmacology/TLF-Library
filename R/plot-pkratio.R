7f3ac62d3f808a18af6c60d5d2d23af3b5c1240e
Unknown <chelle.pierre@gmail.com>
2019-09-03 17:50:19 -0400

    PK ratio plot function

--- R/plot-pkratio.R
+++ R/plot-pkratio.R
@@ -1,4 +1,3 @@
-
 #' Create a PK-Ratio plot
 #'
 #' @param data TODO
@@ -6,33 +5,32 @@
 #' @param dataMapping TODO
 #' @param plotConfiguration TODO
 #'
-#' @return a ggplot object
+#' @return a ggplot graphical object
 #' @export
 #'
 #' @examples
-plotPKRatio <- function(data, metaData, dataMapping, plotConfiguration = NULL) {
+plotPKRatio <- function(data, metaData, dataMapping = NULL, plotConfiguration = NULL) {
+  # If no data mapping or plot configuration is input, use default
   configuration <- plotConfiguration %||% PKRatioPlotConfiguration$new()
+  dataMapping <- dataMapping %||% PKRatioDataMapping$new()
 
   stopifnot("PKRatioDataMapping" %in% class(dataMapping))
   stopifnot("PKRatioPlotConfiguration" %in% class(configuration))
 
   x <- dataMapping$x
   y <- dataMapping$y
-  grouping <- dataMapping$grouping
 
-  plot <- ggplot(data, aes(x = data[[x]], y = data[[y]], color = data[[grouping]])) +
-    labs(title = configuration$title, x = x, y = y, color = grouping) +
-    geom_point()
+  colorGrouping <- getGrouping(data, dataMapping$colorGrouping)
+  sizeGrouping <- getGrouping(data, dataMapping$sizeGrouping)
+  shapeGrouping <- getGrouping(data, dataMapping$shapeGrouping)
 
+  plotObject <- ggplot()
 
-  plot <- addRatioLines(plot, plotConfiguration)
-  plot
-}
+  # Add Plot Configuration layers and PK Ratios
+  plotObject <- plotConfiguration$setWatermark(plotObject)
+  plotObject <- plotConfiguration$defineLabels(plotObject, dataMapping)
+  plotObject <- plotConfiguration$addRatioLines(plotObject)
+  plotObject <- plotObject + geom_point(data = data[, c(dataMapping$x, dataMapping$y, dataMapping$colorGrouping, dataMapping$sizeGrouping, dataMapping$shapeGrouping)], mapping = aes(x = data[, x], y = data[, y], color = colorGrouping, size = sizeGrouping, shape = shapeGrouping))
 
-addRatioLines <- function(plot, plotConfiguration) {
-  plot + geom_hline(yintercept = 1, linetype = "solid", color = "black", size = 2) +
-    geom_hline(yintercept = 0.5, linetype = "dashed", color = "red", size = 1) +
-    geom_hline(yintercept = 2, linetype = "dashed", color = "red", size = 1) +
-    geom_hline(yintercept = 0.75, linetype = "dashed", color = "blue", size = 1) +
-    geom_hline(yintercept = 1.55, linetype = "dashed", color = "blue", size = 1)
+  return(plotObject)
 }
