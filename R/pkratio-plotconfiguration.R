ccff52cf4584c064ffab1303af028cbda2e262ee
Unknown <chelle.pierre@gmail.com>
2019-09-03 17:49:58 -0400

    define pk ratio config with integrated methods for plotting pk ratios

--- R/pkratio-plotconfiguration.R
+++ R/pkratio-plotconfiguration.R
@@ -1,3 +1,48 @@
-PKRatioPlotConfiguration <- R6::R6Class("PKRatioPlotConfiguration",
+#' @title PKRatioPlotConfiguration
+#' @docType class
+#' @description  Plot Configuration for PKRatio
+#' @export
+PKRatioPlotConfiguration <- R6::R6Class(
+  "PKRatioPlotConfiguration",
   inherit = PlotConfiguration,
+  public = list(
+    RatioLinesProperties = data.frame(
+      ratio = c(1, 1.5, 1 / 1.5, 2, 1 / 2),
+      linetype = c("solid", "dashed", "dashed", "dashed", "dashed"),
+      color = c("black", "blue", "blue", "red", "red"),
+      size = c(2, 1, 1, 1, 1)
+    ),
+    initialize = function(title = asLabel("PK Ratio Plot"),
+                          xlabel = asLabel("Age [yrs]"),
+                          ylabel = asLabel("Simulated/Observed Ratio"),
+                          RatioLinesProperties = data.frame(
+                            ratio = c(1, 1.5, 1 / 1.5, 2, 1 / 2),
+                            linetype = c("solid", "dashed", "dashed", "dashed", "dashed"),
+                            color = c("black", "blue", "blue", "red", "red"),
+                            size = c(2, 1, 1, 1, 1)
+                          ),
+                          ...) {
+      super$initialize(
+        title = title,
+        xlabel = xlabel,
+        ylabel = ylabel,
+        ...
+      )
+
+      self$RatioLinesProperties <- RatioLinesProperties
+    },
+
+    addRatioLines = function(plotHandle) {
+      for (RatioIndex in seq(1, length(self$RatioLinesProperties$ratio))) {
+        plotHandle <- plotHandle +
+          geom_hline(
+            yintercept = self$RatioLinesProperties$ratio[RatioIndex],
+            linetype = as.character(self$RatioLinesProperties$linetype[RatioIndex]),
+            color = as.character(self$RatioLinesProperties$color[RatioIndex]),
+            size = self$RatioLinesProperties$size[RatioIndex]
+          )
+      }
+      return(plotHandle)
+    }
+  )
 )
