146c5f4d3e7aad470dab50cd1490ab4eb2819023
Unknown <chelle.pierre@gmail.com>
2019-09-13 19:55:04 -0400

    Add new utils function ifnotnull

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,17 @@
-
+# If left is null, use right argument
 `%||%` <- function(lhs, rhs) {
   if (!is.null(lhs)) {
     lhs
   } else {
     rhs
+  }
+}
+
+# If left is not null, use 2nd argument, else use last argument
+ifnotnull <- function(inputToCheck, outputIfNotNull, outputIfNull = NULL) {
+  if (!is.null(inputToCheck)) {
+    outputIfNotNull
+  } else {
+    outputIfNull
   }
 }
