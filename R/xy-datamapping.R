3bfc7a41c77d3925a6d975c30b02aa978a5c5548
Unknown <chelle.pierre@gmail.com>
2019-09-17 17:09:09 -0400

    Addition of XY Error mapping

--- R/xy-datamapping.R
+++ R/xy-datamapping.R
@@ -13,3 +13,29 @@ XYDataMapping <- R6::R6Class(
     }
   )
 )
+
+
+#' @title XYEDataMapping
+#' @docType class
+#' @description Abstract class for X Y Error Mapping
+#' @export
+XYEDataMapping <- R6::R6Class(
+  "XYEDataMapping",
+  inherit = XYDataMapping,
+  public = list(
+    error = NULL,
+    errorMin = NULL,
+    errorMax = NULL,
+
+    # Example of how to do some other stuff
+    initialize = function(x, y,
+                          error = NULL,
+                          errorMin = NULL,
+                          errorMax = NULL) {
+      super$initialize(x, y)
+      self$error <- error
+      self$errorMin <- error %||% errorMin
+      self$errorMax <- error %||% errorMax
+    }
+  )
+)
