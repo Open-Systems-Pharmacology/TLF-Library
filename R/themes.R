0215a9d47a24350183f98e01b52b7e6013ee037b
Unknown <chelle.pierre@gmail.com>
2019-10-01 11:42:04 -0400

    Update theme

--- R/themes.R
+++ R/themes.R
@@ -7,18 +7,33 @@
 Theme <- R6::R6Class(
   "Theme",
   public = list(
-    title = NULL,
-    subtitle = NULL,
-    xlabel = NULL,
-    ylabel = NULL,
-    watermark = NULL,
+    titleFont = NULL,
+    subtitleFont = NULL,
+    xlabelFont = NULL,
+    ylabelFont = NULL,
+    watermarkFont = NULL,
 
-    initialize = function(colorPalette = labelColorPalette$default, baseSize=12) {
-      self$title <- Font$new(color = colorPalette$title, size = baseSize * 2, fontFace = "bold")
-      self$subtitle <- Font$new(color = colorPalette$subtitle, size = baseSize * 1.5, fontFace = "italic")
-      self$xlabel <- Font$new(color = colorPalette$xlabel, size = baseSize)
-      self$ylabel <- Font$new(color = colorPalette$ylabel, size = baseSize)
-      self$watermark <- Font$new(color = colorPalette$watermark, size = baseSize)
+    aesProperties = NULL,
+
+    pkRatioLinesProperties = NULL,
+
+    initialize = function(labelColors = labelColorPalette$default,
+                              labelBaseSize = 12,
+                              aesProperties = aesProperties$default,
+                              pkRatioLinesProperties = pkRatioLinesPropertiesDefault) {
+
+      # Set font properties of labels
+      self$titleFont <- Font$new(color = labelColors$title, size = labelBaseSize * 2, fontFace = "bold")
+      self$subtitleFont <- Font$new(color = labelColors$subtitle, size = labelBaseSize * 1.5, fontFace = "italic")
+      self$xlabelFont <- Font$new(color = labelColors$xlabel, size = labelBaseSize)
+      self$ylabelFont <- Font$new(color = labelColors$ylabel, size = labelBaseSize)
+      self$watermarkFont <- Font$new(color = labelColors$watermark, size = labelBaseSize)
+
+      # Set the color, shape, size maps
+      self$aesProperties <- aesProperties
+
+      # Set the properties of specific plots
+      self$pkRatioLinesProperties <- pkRatioLinesProperties
     },
 
     setPlotConfiguration = function(PlotConfiguration) {
@@ -31,24 +46,97 @@ Theme <- R6::R6Class(
   )
 )
 
+## -------------------------------------------------
 # Define color palettes for plot labels
 labelColorPalette <- enum(c("default", "tlf", "bw"))
 labelColorPalette$default <- list(title = "darkblue", subtitle = "black", xlabel = "black", ylabel = "black", watermark = "blue")
 labelColorPalette$tlf <- list(title = "darkblue", subtitle = "black", xlabel = "black", ylabel = "black", watermark = "green")
 labelColorPalette$bw <- list(title = "black", subtitle = "black", xlabel = "black", ylabel = "black", watermark = "grey50")
 
+
+## -------------------------------------------------
+# Define default colorMaps for plots
+aesProperties <- enum(c("default", "tlf", "bw"))
+aesProperties$default <- list(
+  color = c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
+  size = seq(1, 10),
+  shape = seq(1, 10), # c("circle", "square", "diamond", "triangle"),
+  linetype = c("solid", "dashed", "dotted")
+)
+
+aesProperties$tlf <- list(
+  color = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7"),
+  size = seq(1, 10),
+  shape = c("circle", "square", "diamond", "triangle"),
+  linetype = c("solid", "dashed", "dotted")
+)
+
+aesProperties$bw <- list(
+  color = c("#000000", "#999999", "#555555"),
+  size = seq(1, 10),
+  shape = c("circle", "square", "diamond", "triangle"),
+  linetype = c("solid", "dashed", "dotted")
+)
+
+aesProperties$big <- aesProperties$default
+aesProperties$big$size <- seq(4, 20, 2)
+
+## -------------------------------------------------
+# Define PK Ratio Lines Properties
+pkRatioLinesPropertiesDefault <- data.frame(
+  value = c(1, 1.5, 1 / 1.5, 2, 1 / 2),
+  linetype = c("solid", "dashed", "dashed", "dashed", "dashed"),
+  color = c("black", "blue", "blue", "red", "red"),
+  size = c(2, 1, 1, 1, 1)
+)
+
+# Otherwise linetype and color are assumed as levels
+pkRatioLinesPropertiesDefault$linetype <- as.character(pkRatioLinesPropertiesDefault$linetype)
+pkRatioLinesPropertiesDefault$color <- as.character(pkRatioLinesPropertiesDefault$color)
+
+# Black and White properties
+pkRatioLinesPropertiesBW <- pkRatioLinesPropertiesDefault
+pkRatioLinesPropertiesBW$color <- as.character(c("#000000", "#555555", "#555555", "#999999", "#999999"))
+pkRatioLinesPropertiesBW$linetype <- as.character(c("solid", "dashed", "dashed", "dotted", "dotted"))
+
+# Big theme properties
+pkRatioLinesPropertiesBig <- pkRatioLinesPropertiesDefault
+pkRatioLinesPropertiesBig$size <- pkRatioLinesPropertiesDefault$size * 2
+
+## -------------------------------------------------
 # Definition of default theme
-defaultTheme <- Theme$new(colorPalette = labelColorPalette$default, baseSize = 12)
+defaultTheme <- Theme$new(
+  labelColors = labelColorPalette$default,
+  labelBaseSize = 12,
+  aesProperties = aesProperties$default
+)
 
 # Definition of tlf theme
-tlfTheme <- Theme$new(colorPalette = labelColorPalette$tlf, baseSize = 12)
+tlfTheme <- Theme$new(
+  labelColors = labelColorPalette$tlf,
+  labelBaseSize = 12,
+  aesProperties = aesProperties$tlf
+)
 
 # Definition of black and white theme
-bwTheme <- Theme$new(colorPalette = labelColorPalette$bw, baseSize = 12)
+bwTheme <- Theme$new(
+  labelColors = labelColorPalette$bw,
+  labelBaseSize = 12,
+  aesProperties = aesProperties$bw,
+  pkRatioLinesProperties = pkRatioLinesPropertiesBW
+)
 
 # Definition of black and white theme
-bigTheme <- Theme$new(colorPalette = labelColorPalette$default, baseSize = 16)
+bigTheme <- Theme$new(
+  labelColors = labelColorPalette$default,
+  labelBaseSize = 16,
+  aesProperties = aesProperties$default,
+  pkRatioLinesProperties = pkRatioLinesPropertiesBig
+)
 
+
+## -------------------------------------------------
+# Util functions
 useTheme <- function(theme) {
   tlfEnv$currentTheme <- theme
 }
