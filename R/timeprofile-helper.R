174cd247d6314de5e14f4179005bf81f450a9b01
Abdullah <aoh123@gmail.com>
2019-10-09 18:49:08 -0400

    Update timeprofile-helper.R
    dataframe data and timeColumnName must be supplied.  aggregationFunctionsVector coerced into vector form.  Fixed colnames bug.

--- R/timeprofile-helper.R
+++ R/timeprofile-helper.R
@@ -1,5 +1,3 @@
-library(ggplot2)
-graphics.off()
 TimeProfileHelper <- R6::R6Class(
   "TimeProfileHelper",
   public = list(
@@ -10,8 +8,8 @@ TimeProfileHelper <- R6::R6Class(
     aggregationFunctionsVector = NULL,
     aggregationFunctionNames = NULL,
     dfHelper = NULL,
-    initialize = function(data = NULL,
-                              timeColumnName = NULL,
+    initialize = function(data,
+                              timeColumnName,
                               groupingColumnNames = NULL,
                               valuesColumnNames = NULL,
                               aggregationFunctionsVector = NULL,
@@ -20,13 +18,13 @@ TimeProfileHelper <- R6::R6Class(
       self$timeColumnName <- timeColumnName
       self$groupingColumnNames <- groupingColumnNames
       self$valuesColumnNames <- valuesColumnNames
-      self$aggregationFunctionsVector <- aggregationFunctionsVector
+      self$aggregationFunctionsVector <- c(aggregationFunctionsVector)
       self$aggregationFunctionNames <- aggregationFunctionNames
       self$generateAggregatedValues()
     },
 
     applyAggregationFunctions = function(x) {
-      # input the vector of aggregated x into each aggregation function y and return the results in a vection res
+      # input the vector of aggregated x into each aggregation function y and return the results in a vector res
       res <- sapply(self$aggregationFunctionsVector, function(y) {
         y(x)
       })
@@ -34,6 +32,7 @@ TimeProfileHelper <- R6::R6Class(
     },
 
     generateAggregatedValues = function() {
+
       xGroupingColNames <- c(self$timeColumnName, self$groupingColumnNames) # Get names of grouping columns and groups then into a vector xGroupingColNames
       xGroupingCols <- lapply(xGroupingColNames, function(x) {
         self$data[[x]]
@@ -48,9 +47,8 @@ TimeProfileHelper <- R6::R6Class(
       # Then for each such vector apply the functions in the vector aggregationFunctionsVector.  Output over all rows in xGroupingCols and functions in aggregationFunctionsVector forms a matrix
       # xGroupingCols and the output matrix are combined to form one dataframe called dfHelper.
 
-      colnames(dfHelper)[seq(1, length(xGroupingColNames))] <- xGroupingColNames # Rename columns corresponding to xGroupingCols in dfHelper
-
       dfHelper <- cbind(dfHelper[, seq(1, ncol(dfHelper) - 1)], as.data.frame(dfHelper$Simulated)) # Split last (matrix) column of dfHelper into a series of columns, each corresponding to a different function in aggregationFunctionsVector
+      colnames(dfHelper)[seq(1, length(xGroupingColNames))] <- xGroupingColNames # Rename columns corresponding to xGroupingCols in dfHelper
       resultCols <- seq(length(xGroupingColNames) + 1, length(xGroupingColNames) + length(self$aggregationFunctionsVector)) # Rename the split columns according to the names of the functions supplied in aggregationFunctionNames
       colnames(dfHelper)[resultCols] <- self$aggregationFunctionNames
       self$dfHelper <- dfHelper
