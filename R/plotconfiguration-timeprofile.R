d69e6e337048f81c6eff8b72c5753fa477d406ac
Unknown <chelle.pierre@gmail.com>
2019-10-17 10:30:41 -0400

    Fix time profile obsolete Groupings

--- R/plotconfiguration-timeprofile.R
+++ R/plotconfiguration-timeprofile.R
@@ -8,12 +8,8 @@ TimeProfilePlotConfiguration <- R6::R6Class(
 
   public = list(
     LLOQLinesProperties = NULL,
-    colorGrouping = NULL,
-    linetypeGrouping = NULL,
-
+    
     initialize = function(LLOQLinesProperties = tlfEnv$currentTheme$LLOQLinesProperties,
-                              colorGrouping = NULL,
-                              linetypeGrouping = NULL,
                               title = "Time Profile Plot",
                               subtitle = paste("Date:", format(Sys.Date(), "%y-%m-%d")),
                               xlabel = NULL,
@@ -35,15 +31,7 @@ TimeProfilePlotConfiguration <- R6::R6Class(
       )
 
       self$LLOQLinesProperties <- LLOQLinesProperties
-
-      if (!is.null(dataMapping$groupings)) {
-        self$colorGrouping <- dataMapping$groupings[["color"]]$groupingName[[1]]
-        self$linetypeGrouping <- dataMapping$groupings[["linetype"]]$groupingName[[1]]
-      }
-
-      # Overwrite grouping if config is different from Mapping
-      self$colorGrouping <- colorGrouping %||% self$colorGrouping
-      self$linetypeGrouping <- linetypeGrouping %||% self$linetypeGrouping
+      
     },
 
     addLLOQLines = function(LLOQLines, plotObject) {
@@ -64,46 +52,47 @@ TimeProfilePlotConfiguration <- R6::R6Class(
     addTimeProfiles = function(plotObject, data, metaData, dataMapping) {
       mapData <- dataMapping$getMapData(data, metaData)
 
-      # If no grouping is defined, the aesthetic variables still need to be included
-      # with a dummy name to be removed, otherwise it is impossible to modify them later on.
-      if (is.null(self$linetypeGrouping)) {
-        linetype <- "linetype"
-      }
-      if (is.null(self$colorGrouping)) {
-        color <- "color"
-      }
-
+      color <- self$legend$titles$color %||% "none"
+      shape <- self$legend$titles$shape %||% "none"
+      linetype <- self$legend$titles$linetype %||% "none"
+      fill <- self$legend$titles$fill %||% "none"
+      
+      # Define dummy unique value for grouping
+      # Allows further modification of the aesthtic property
+      if (is.null(self$legend$titles$color)){mapData[, color] <- as.factor(1)}
+      if (is.null(self$legend$titles$shape)){mapData[, shape] <- as.factor(1)}
+      if (is.null(self$legend$titles$linetype)){mapData[, linetype] <- as.factor(1)}
+      if (is.null(self$legend$titles$fill)){mapData[, fill] <- as.factor(1)}
+      
       if (!is.null(dataMapping$y)) {
         if (!is.null(dataMapping$yMin) && !is.null(dataMapping$yMax)) {
           plotObject <- plotObject + ggplot2::geom_errorbar(
-            data = mapData,
-            mapping = aes(x = x, ymin = yMin, ymax = yMax, color = color, linetype = linetype),
+            mapping = aes(x = mapData$x, ymin = mapData$yMin, ymax = mapData$yMax, 
+                          color = mapData[,color], linetype = mapData[,linetype]),
             size = 1,
             show.legend = TRUE
           )
         }
         plotObject <- plotObject + ggplot2::geom_line(
-          data = mapData,
-          mapping = aes(x = x, y = y, color = color, linetype = linetype),
+          mapping = aes(x = mapData$x, y = mapData$y, 
+                        color = mapData[,color], linetype = mapData[,linetype]),
           size = 1,
           show.legend = TRUE
         )
       } else {
         plotObject <- plotObject + ggplot2::geom_ribbon(
-          data = mapData,
-          mapping = aes(x = x, ymin = yMin, ymax = yMax, fill = color),
+          mapping = aes(x = mapData$x, ymin = mapData$yMin, ymax = mapData$yMax, 
+                        fill = mapData[,fill]),
           show.legend = TRUE
         )
       }
-
+      
       # If no grouping is defined, remove the dummy aesthtic name from the legend
-      if (is.null(self$linetypeGrouping)) {
-        plotObject <- plotObject + guides(linetype = "none")
-      }
-      if (is.null(self$colorGrouping)) {
-        plotObject <- plotObject + guides(color = "none")
-      }
-
+      if ("none" %in% linetype) {plotObject <- plotObject + guides(linetype = "none")}
+      if ("none" %in% color) {plotObject <- plotObject + guides(color = "none")}
+      if ("none" %in% fill) {plotObject <- plotObject + guides(fill = "none")}
+      if ("none" %in% shape) {plotObject <- plotObject + guides(shape = "none")}
+      
       return(plotObject)
     }
   )
