66ade4cedf88f2e15b48a59b6ab9dba02e21dd9a
Abdullah <aoh123@gmail.com>
2019-10-20 12:09:08 -0400

    added comments

--- R/aggregation-summary.R
+++ R/aggregation-summary.R
@@ -53,6 +53,16 @@ aggregationSummary <- R6::R6Class(
         self$dfHelper[[self$aggregationFunctionNames[n]]]<-dF
       }
 
+      #Splits the dataframe data into subsets defined by uniqe combinations of elements in the columns xColumnNames  and groupingColumnNames.
+      #Applies functions defined in aggregationFunctionsVector to column yColumnName.
+      #Returns a list of dataframes, one dataframe for each function listed in aggregationFunctionsVector.
+      #Each dataframe in list element is named after the function's corresponding string in aggregationFunctionNames.
+      #The summary statistic column name in each dataframe is the same as the name of the dataframe in the returned list.
+
+      self$groupingColumnNames <- groupingColumnNames
+      self$yColumnName <- yColumnName
+      self$aggregationFunctionsVector <- c(aggregationFunctionsVector)
+
       # For each unique combination of entries in the rows of the grouping columns xGroupingCols, get corresponding values from yValuesCol and group these values into a vector.
       # Then for each such vector apply the functions in the vector aggregationFunctionsVector.  Output over all rows in xGroupingCols and functions in aggregationFunctionsVector forms a matrix
       # xGroupingCols and the output matrix are combined to form one dataframe called dfHelper.
