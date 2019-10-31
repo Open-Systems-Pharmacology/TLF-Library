4e5ab4f08a5f88544553029305197fa67c12c707
Abdullah <aoh123@gmail.com>
2019-10-30 23:24:07 -0400

    Update datamapping-histogram.R

--- R/datamapping-histogram.R
+++ R/datamapping-histogram.R
@@ -4,29 +4,13 @@
 #' @export
 HistogramDataMapping <- R6::R6Class(
   "HistogramDataMapping",
-  inherit = XDataMapping,
+  inherit = XYGDataMapping,
   public = list(
-    groupings = NULL, # R6 Class of GroupMappings
+      verticalLineGroupings = NULL,
 
-    initialize = function(x, y, groupings = NULL) {
-      super$initialize(x, y)
-
-      self$groupings <- groupings %||% Groupings$new()
-    },
-
-    getMapData = function(data, metaData = NULL) {
-      x <- data[, self$x] 
-
-      self$data <- data.frame(x)
-
-      # All possible Groupings are listed in the enum LegendTypes
-      for (groupType in LegendTypes) {
-        if (!is.null(self$groupings[[groupType]]$group)) {
-          grouping <- self$groupings[[groupType]]
-          self$data[, grouping$label] <- grouping$getCaptions(data, metaData)
-        }
+      initialize = function( x, y=NULL , groupings=NULL , verticalLineGroupings = NULL ) {
+        super$initialize(x,y,groupings)
+        self$verticalLineGroupings <- verticalLineGroupings
       }
-      return(self$data)
-    }
   )
 )
