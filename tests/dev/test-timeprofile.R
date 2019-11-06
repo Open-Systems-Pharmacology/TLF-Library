af8a84e06c7a88c653d682e42df20c167eeaa25a
Unknown <chelle.pierre@gmail.com>
2019-11-06 11:17:45 -0500

    Add test examples to dev for time profiles

--- tests/dev/test-timeprofile.R
+++ tests/dev/test-timeprofile.R
@@ -7,7 +7,7 @@ library(tlf)
 
 # Set your folder to test folder:
 # setwd("C:/Design2Code/TLF-Playground/TLF-Playground/R/")
-useTheme(bigTheme)
+useTheme(defaultTheme)
 
 # -------------------------------------------------
 # Get the data and metadata for PK Ratio plot
@@ -16,37 +16,21 @@ nPopulation <- 20
 
 # -------------------------------------------------
 
-testData <- list(
-  observedData = data.frame(
-    StudyID = c(1, 1, 1),
-    Time = c(1, 10, 24),
-    Value = c(10, 5, 2.5),
-    Error = c(2, 1, 0.5)
-  ),
-  simulatedResults = data.frame(
+testData = data.frame(
     IndivdualID = rep(1, 24),
     Time = seq(1, 24),
     Value = 10 * exp(-0.06 * seq(1, 24))
   )
-)
-
-testMetaData <- list()
-testMetaData$observedData <- getMetaData(testData$observedData)
-testMetaData$simulatedResults <- getMetaData(testData$simulatedResults)
-testMetaData$observedData$Value <- list(unit = "mg/L", dimension = "Concentration")
-testMetaData$simulatedResults$Value <- list(unit = "mg/L", dimension = "Concentration")
 
 
+testMetaData <- list(IndivdualID = list("unit"= "", "dimension"=""),
+                     Time = list("unit"= "min", "dimension"="Time"),
+                     Value= list("unit"= "mg/L", "dimension"="Concentration"))
 
 # -------------------------------------------------
 # Define Default plot Configuration & Mapping from R6 class for PK Ratio
-testObservationMapping <- XYEDataMapping$new(x = "Time", y = "Value", error = "Error")
 
-testDataMapping <- TimeProfileDataMapping$new(
-  simulationSets = "simulatedResults",
-  observationSets = "observedData",
-  observationMapping = testObservationMapping
-)
+testDataMapping <- TimeProfileDataMapping$new(x = "Time", y = "Value")
 
 # Renaming of Label from initialize
 testConfiguration <- TimeProfilePlotConfiguration$new(
