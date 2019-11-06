0111c0d15e6b4b3835a2f90dba7e098d3e02925a
Unknown <chelle.pierre@gmail.com>
2019-11-06 08:53:44 -0500

    Add check map data method + include dummy variable for aesthetics

--- R/datamapping-xygroup.R
+++ R/datamapping-xygroup.R
@@ -21,11 +21,39 @@ XYGDataMapping <- R6::R6Class(
 
       # All possible Groupings are listed in the enum LegendTypes
       for (groupType in LegendTypes) {
-        if (!is.null(self$groupings[[groupType]]$group)) {
-          grouping <- self$groupings[[groupType]]
+        if (!is.null(self$groupingMapping[[groupType]]$group)) {
+          grouping <- self$groupingMapping[[groupType]]
           self$data[, grouping$label] <- grouping$getCaptions(data, metaData)
         }
       }
+      return(self$data)
+    },
+
+    checkMapData = function(data, metaData = NULL) {
+      validateMapping(self$x, data)
+      validateMapping(self$y, data)
+
+      self$data <- data[, c(self$x, self$y)]
+
+      # All possible Groupings are listed in the enum LegendTypes
+      for (groupType in LegendTypes) {
+        if (!is.null(self$groupMapping[[groupType]]$group)) {
+          grouping <- self$groupMapping[[groupType]]
+
+          groupVariables <- grouping$group
+          if (isOfType(groupVariables, "data.frame")) {
+            # Last group variable is the label in group data.frames
+            # and need to be removed from the check
+            groupVariables <- names(groupVariables)
+            groupVariables <- utils::head(groupVariables, -1)
+          }
+          validateMapping(groupVariables, data)
+
+          self$data[, grouping$label] <- grouping$getCaptions(data, metaData)
+        }
+      }
+      # Dummy variable for default aesthetics
+      self$data$defaultAes <- factor("")
       return(self$data)
     }
   )
