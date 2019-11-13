dec9569109ce4ae4088dfa2d730582be8fef6b2e
Unknown <chelle.pierre@gmail.com>
2019-11-13 12:43:08 -0500

    update based on check() warnings

--- R/get-boxwhisker-measure.R
+++ R/get-boxwhisker-measure.R
@@ -18,10 +18,9 @@ getBoxWhiskerMeasure <- function(data, dataMapping = NULL, y = NULL, group = NUL
 
   # If no data mapping is input, use default
   variableNames <- names(data)
-  group <- group %||% head(variableNames, 1)
-  y <- y %||% tail(variableNames, 1)
+  group <- group
+  y <- y %||% utils::tail(variableNames, 1)
   dataMapping <- dataMapping %||% BoxWhiskerDataMapping$new(
-    data = data,
     x = group,
     y = y
   )
@@ -29,8 +28,10 @@ getBoxWhiskerMeasure <- function(data, dataMapping = NULL, y = NULL, group = NUL
   validateIsOfType(dataMapping, "BoxWhiskerDataMapping")
 
   # Redfine group and y while removing NA values
-  group <- data[!is.na.data.frame(data[, dataMapping$y]), dataMapping$x]
   y <- data[!is.na.data.frame(data[, dataMapping$y]), dataMapping$y]
+  group <- ifnotnull(dataMapping$x,
+                     data[!is.na.data.frame(data[, dataMapping$y]), dataMapping$x],
+                     as.factor(rep("", length(y))))
 
   summary <- tapply(y, group, FUN = function(x) {
     summaryStat(x, quantiles)
@@ -57,8 +58,8 @@ getBoxWhiskerMeasure <- function(data, dataMapping = NULL, y = NULL, group = NUL
 
 summaryStat <- function(x, quantiles = c(0.05, 0.25, 0.5, 0.75, 0.95), na.rm = FALSE) {
   n <- length(x)
-  summaryQuantiles <- as.numeric(quantile(x, probs = quantiles))
-  summaryStat <- c(mean(x, na.rm = na.rm), sd(x, na.rm = na.rm), exp(mean(log(x), na.rm = na.rm)), exp(sd(log(x), na.rm = na.rm)))
+  summaryQuantiles <- as.numeric(stats::quantile(x, probs = quantiles))
+  summaryStat <- c(mean(x, na.rm = na.rm), stats::sd(x, na.rm = na.rm), exp(mean(log(x), na.rm = na.rm)), exp(stats::sd(log(x), na.rm = na.rm)))
 
   summary <- c(n, summaryQuantiles, summaryStat)
 
