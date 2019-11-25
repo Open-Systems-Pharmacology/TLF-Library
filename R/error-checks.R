ff7ce71f30926233d60474e1d40215bdabb390a3
Unknown <chelle.pierre@gmail.com>
2019-11-25 08:00:56 -0500

    Check for conflicting input

--- R/error-checks.R
+++ R/error-checks.R
@@ -135,10 +135,37 @@ validateMapping <- function(mapping, data, nullAllowed = FALSE) {
     return()
   }
 
-  validateIsString(mapping)
-  variableNames <- names(data)
+  if (isOfType(mapping, "numeric")) {
+    if (ncol(data) >= mapping) {
+      return()
+    }
+    stop(messages$errorExceedLength(mapping, ncol(data)))
+  }
+  if (isOfType(mapping, "character")) {
+    variableNames <- names(data)
+    validateIsIncluded(mapping, variableNames)
+    return()
+  }
 
-  validateIsIncluded(mapping, variableNames)
+  stop(messages$errorWrongType(mapping, class(mapping), c("numeric", "character")))
+}
 
-  return()
+#' Check that 2 conflicting inputs can't be provided at the same time
+#'
+#' @param eitherInput Input or list of inputs
+#' @param orInput Input or list of inputs
+validateEitherOrNullInput <- function(eitherInput, orInput) {
+  # Convert input to list of input if not already as list
+  if (!isOfType(eitherInput, "list")) {
+    eitherInput <- list(eitherInput)
+  }
+  if (!isOfType(orInput, "list")) {
+    orInput <- list(orInput)
+  }
+
+  # False if one element from input is not null
+  if (as.logical(min(sapply(eitherInput, is.null))) || as.logical(min(sapply(orInput, is.null)))) {
+    return()
+  }
+  stop(messages$errorConflictingInput(names(eitherInput), names(orInput)))
 }
