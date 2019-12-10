a3548abc8d1865fe6876fa4c5ab1b120b2c0284a
Unknown <chelle.pierre@gmail.com>
2019-12-10 12:16:43 -0500

    Document font and label + allow font input as input

--- R/font.R
+++ R/font.R
@@ -1,7 +1,16 @@
 #' @title Font
 #' @docType class
-#' @description
-#' Font class defining properties of a text (size, color, font)
+#' @description Font properties for ggplot labels
+#' @field size numeric
+#' @field color character
+#' @field fontFamily character
+#' @field fontFace character
+#' @section Methods:
+#' \describe{
+#' \item{new(...)}{Initialize Font}
+#' \item{print(...)}{Print Font properties}
+#' \item{setFont()}{Create element_text for ggplot with Font properties}
+#' }
 #' @include utils.R
 #' @export
 Font <- R6::R6Class(
@@ -20,6 +29,14 @@ Font <- R6::R6Class(
       self$color <- color
       self$fontFamily <- fontFamily
       self$fontFace <- fontFace
+    },
+
+    print = function() {
+      cat("size:", self$size, "\n", sep = " ")
+      cat("color:", self$color, "\n", sep = " ")
+      cat("fontFamily:", self$fontFamily, "\n", sep = " ")
+      cat("fontFace:", self$fontFace, "\n", sep = " ")
+      invisible(self)
     },
 
     setFont = function() {
--- R/label.R
+++ R/label.R
@@ -1,7 +1,14 @@
 #' @title Label
 #' @docType class
-#' @description
-#' Label class associate font to a text
+#' @description Properties for ggplot labels
+#' @field text character
+#' @field font Font object
+#' @section Methods:
+#' \describe{
+#' \item{new(...)}{Initialize Label}
+#' \item{print(...)}{Print Label properties}
+#' \item{setFontProperties(color, size, fontFamily, fontFace)}{Set fields of font object}
+#' }
 #' @include utils.R
 #' @export
 Label <- R6::R6Class(
@@ -10,10 +17,36 @@ Label <- R6::R6Class(
     text = NULL,
     font = NULL,
 
-    initialize = function(text = "", font = NULL) {
+    initialize = function(text = "",
+                              font = NULL,
+                              color = NULL,
+                              size = NULL,
+                              fontFace = NULL,
+                              fontFamily = NULL) {
       self$text <- text
-      validateIsOfType(font, Font, nullAllowed = TRUE)
-      self$font <- font %||% Font$new()
+      validateEitherOrNullInput(
+        list("font" = font),
+        list(
+          "color" = color,
+          "size" = size,
+          "fontFace" = fontFace,
+          "fontFamily" = fontFamily
+        )
+      )
+
+      self$font <- font %||% Font$new(
+        color = color,
+        size = size,
+        fontFace = fontFace,
+        fontFamily = fontFamily
+      )
+      validateIsOfType(self$font, Font)
+    },
+
+    print = function() {
+      cat("text:", self$text, "\n", sep = " ")
+      cat("font:", self$font, "\n", sep = " ")
+      invisible(self)
     },
 
     setFontProperties = function(color = self$font$color,
@@ -24,11 +57,6 @@ Label <- R6::R6Class(
       self$font$size <- size
       self$font$fontFamily <- fontFamily
       self$font$fontFace <- fontFace
-    },
-
-    print = function() {
-      print(self$text)
-      invisible(self)
     }
   )
 )
