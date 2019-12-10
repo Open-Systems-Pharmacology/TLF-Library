a7ad9aa551f4f801605325f8285d1b42e0a57a85
Unknown <chelle.pierre@gmail.com>
2019-12-10 12:15:19 -0500

    Add some more test to labels and font

--- tests/testthat/test-asLabel.R
+++
@@ -1,7 +0,0 @@
-context("asLabel")
-
-test_that("asLabel gives always a Label class object", {
-  x <- asLabel("a")
-  expect_is(x, "Label")
-  expect_is(asLabel(x), "Label")
-})
---
+++ tests/testthat/test-font-label.R
@@ -0,0 +1,33 @@
+context("Font and label")
+
+test_that("asLabel gives always a Label class object", {
+  x <- asLabel("a")
+  expect_is(x, "Label")
+  expect_is(asLabel(x), "Label")
+})
+
+test_that("Font default works", {
+  defaultFont <- Font$new()
+
+  expect_is(defaultFont, "Font")
+  expect_is(defaultFont$size, "numeric")
+  expect_is(defaultFont$color, "character")
+  expect_is(defaultFont$fontFace, "character")
+  expect_is(defaultFont$fontFamily, "character")
+})
+
+test_that("Label default works", {
+  defaultLabel <- Label$new()
+
+  expect_is(defaultLabel, "Label")
+  expect_is(defaultLabel$text, "character")
+  expect_is(defaultLabel$font, "Font")
+})
+
+test_that("Label gives error when initialized with wrong arguments", {
+  expect_error(Label$new(font = ""))
+  expect_error(Label$new(
+    font = Font$new(color = "blue"),
+    color = "red"
+  ))
+})
