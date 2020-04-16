e49e40aeda10c41251b41f2983821e8bdb708efc
Unknown <chelle.pierre@gmail.com>
2020-04-16 12:55:50 -0400

    Method to set watermark of a ggplot
    There is a addWatermark method already implement which was pasting a watermrk, so you can pile them up.
    This one actually updates the watermark layer

--- R/utilities-watermark.R
+++ R/utilities-watermark.R
@@ -5,7 +5,7 @@
 #' @param angle Angle in degree from horizontal of the watermark label. Default angle is 30 degrees.
 #' @param alpha Transparency of the watermark label.
 #' Alpha is a numeric between 0 and 1: 0 label is totally transparent, 1 label is totally opaque.
-#' Default alpha is 0.6.
+#' Default alpha is 0.4.
 #' @return \code{plotObject} ggplot object to which the watermark is added.
 #' @description
 #' addWatermark creates a ggplot grob based on the label text and its font properties.
@@ -34,7 +34,11 @@
 #'     angle = angle, alpha = 1
 #'   )
 #' }
-addWatermark <- function(plotObject, label, angle = 30, alpha = 0.4) {
+addWatermark <- function(plotObject,
+                         label,
+                         angle = 30,
+                         alpha = 0.4) {
+  validateIsOfType(plotObject, "ggplot")
   # Ensure label is a Label class
   label <- asLabel(label)
 
@@ -43,6 +47,63 @@ addWatermark <- function(plotObject, label, angle = 30, alpha = 0.4) {
   plotObject <- plotObject + ggplot2::annotation_custom(grob = watermark, xmin = -Inf, ymin = -Inf, xmax = Inf, ymax = Inf)
 
   return(plotObject)
+}
+
+#' @title setWatermark
+#' @param plotObject ggplot object to which the watermark is set
+#' @param watermark character or Label class object
+#' @param angle Angle in degree from horizontal of the watermark label. Default angle is 30 degrees.
+#' @param alpha Transparency of the watermark label.
+#' Alpha is a numeric between 0 and 1: 0 label is totally transparent, 1 label is totally opaque.
+#' Default alpha is 0.6.
+#' @return ggplot object to which the watermark is added.
+#' @import  ggplot2
+#' @export
+setWatermark <- function(plotObject,
+                         watermark = NULL,
+                         angle = 30,
+                         alpha = 0.4) {
+  validateIsOfType(plotObject, "ggplot")
+  # Angle and alpha can be added as a part of plot configuration later on:
+  # For this create Watermark R6 class inheriting from Label and which gets fields angle and alpha
+  validateIsOfType(watermark, c("Label", "character"), nullAllowed = TRUE)
+
+  # Clone tlfConfiguration into a new plot object
+  # Prevents update of R6 class being spread to plotObject
+  newPlotObject <- plotObject
+  newPlotObject$tlfConfiguration <- plotObject$tlfConfiguration$clone(deep = TRUE)
+
+  # R6 class not cloned will spread modifications into newPlotObject$tlfConfiguration$background$watermark
+  watermarkConfiguration <- newPlotObject$tlfConfiguration$background$watermark
+
+  if (!is.null(watermark)) {
+    if (isOfType(watermark, "character")) {
+      watermark <- asLabel(watermark, watermarkConfiguration$font)
+    }
+  }
+
+  watermarkConfiguration <- watermark %||% watermarkConfiguration
+
+  # If plot is initialized, addWatermark otherwise update watermark
+  if (length(plotObject$layers) == 0) {
+    newPlotObject <- addWatermark(newPlotObject,
+      label = watermarkConfiguration,
+      angle = angle,
+      alpha = alpha
+    )
+    return(newPlotObject)
+  }
+
+  # Using initializePlot, watermark will always be as first layer
+  dummyPlot <- addWatermark(ggplot2::ggplot(),
+    label = watermarkConfiguration,
+    angle = angle,
+    alpha = alpha
+  )
+
+  newPlotObject$layers[[1]] <- dummyPlot$layer[[1]]
+
+  return(newPlotObject)
 }
 
 #' @title createWatermarkGrob
