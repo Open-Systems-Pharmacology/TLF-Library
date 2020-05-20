18997e5d97b49b2325cdd265345b6a2ae5472f22
Unknown <chelle.pierre@gmail.com>
2020-05-20 09:07:24 -0400

    Update tests

--- tests/testthat/test-pkratio.R
+++ tests/testthat/test-pkratio.R
@@ -6,32 +6,20 @@ load("pkRatioDataExample.RData")
 useTheme(tlfTheme)
 
 test_that("plotPKRatio() function works properly", {
-  pkrp <- plotPKRatio(
-    data = data.frame(x = c(1, 2, 10, 20, 100),
-                      y = c(1, 3, 4, 0.5, 0.2))
-  )
+  pkrp <- plotPKRatio(data = data.frame(
+    x = c(1, 2, 10, 20, 100),
+    y = c(1, 3, 4, 0.5, 0.2)
+  ))
 
   expect_is(pkrp, "ggplot")
 
+  expect_error(plotPKRatio(), 'argument "data" is missing, with no default')
   expect_error(
-    plotPKRatio(),
-    'argument "data" is missing, with no default'
-  )
-  expect_error(
-    plotPKRatio(
-      data = pkRatioData,
-      dataMapping = list(
-        x = "Age",
-        y = "Ratio"
-      )
-    ),
+    plotPKRatio(data = pkRatioData, dataMapping = list(x = "Age", y = "Ratio")),
     "argument 'dataMapping' is of type 'list', but expected 'PKRatioDataMapping'!"
   )
   expect_error(
-    plotPKRatio(
-      data = pkRatioData,
-      plotConfiguration = list(title = "PK Ratio Plot")
-    ),
+    plotPKRatio(data = pkRatioData, plotConfiguration = list(title = "PK Ratio Plot")),
     "argument 'plotConfiguration' is of type 'list', but expected 'PKRatioPlotConfiguration'!"
   )
 })
@@ -40,29 +28,31 @@ test_that("PK Ratio default settings work", {
   pkRatioMapping <- PKRatioDataMapping$new()
   pkRatioConfig <- PKRatioPlotConfiguration$new()
   pkRatioConfigAsCalledInPlot <- PKRatioPlotConfiguration$new(
-    data = pkRatioData[,c("Age", "Ratio")],
+    data = pkRatioData[, c("Age", "Ratio")],
     metaData = pkRatioMetaData
   )
 
   expect_null(pkRatioMapping$x)
   expect_null(pkRatioMapping$y)
-  expect_equal(pkRatioMapping$pkRatioLines, c(1, 1.5, 1 / 1.5, 2, 1 / 2))
+  expect_equal(pkRatioMapping$pkRatioValues, list(
+    pkRatio1 = 1,
+    pkRatio2 = c(1.5, 1 / 1.5),
+    pkRatio3 = c(2, 1 / 2)
+  ))
 
-  expect_is(pkRatioConfig$title, "Label")
-  expect_is(pkRatioConfig$subtitle, "Label")
-  expect_is(pkRatioConfig$xlabel, "Label")
-  expect_is(pkRatioConfig$ylabel, "Label")
+  expect_is(pkRatioConfig$labels$title, "Label")
+  expect_is(pkRatioConfig$labels$subtitle, "Label")
+  expect_is(pkRatioConfig$labels$xlabel, "Label")
+  expect_is(pkRatioConfig$labels$ylabel, "Label")
   expect_is(pkRatioConfig$background$watermark, "Label")
 
-  expect_equal(pkRatioConfig$title$text, "PK Ratio Plot")
-  expect_equal(pkRatioConfig$subtitle$text, paste("Date:", format(Sys.Date(), "%y-%m-%d")))
-  expect_equal(pkRatioConfig$xlabel$text, "")
-  expect_equal(pkRatioConfig$ylabel$text, "")
-  
-  expect_equal(pkRatioConfigAsCalledInPlot$title$text, "PK Ratio Plot")
-  expect_equal(pkRatioConfigAsCalledInPlot$subtitle$text, paste("Date:", format(Sys.Date(), "%y-%m-%d")))
-  expect_equal(pkRatioConfigAsCalledInPlot$xlabel$text, "Age [yrs]")
-  expect_equal(pkRatioConfigAsCalledInPlot$ylabel$text, "Ratio")
+  expect_null(pkRatioConfig$labels$title$text)
+  expect_null(pkRatioConfig$labels$subtitle$text)
+
+  expect_null(pkRatioConfigAsCalledInPlot$labels$title$text)
+  expect_null(pkRatioConfigAsCalledInPlot$labels$subtitle$text)
+  expect_equal(pkRatioConfigAsCalledInPlot$labels$xlabel$text, "Age [yrs]")
+  expect_equal(pkRatioConfigAsCalledInPlot$labels$ylabel$text, "Ratio")
 })
 
 test_that("PK Ratio typical test works", {
