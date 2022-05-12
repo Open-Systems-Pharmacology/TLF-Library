4008847aa469413e14df4168a4a7c9b1c958def4
Unknown <chelle.pierre@gmail.com>
2022-05-12 05:01:47 -0400

    Also test default scales (lin/log/discrete)

--- tests/testthat/test-expand.R
+++ tests/testthat/test-expand.R
@@ -1,5 +1,7 @@
+context("Default values for plot configuration expand and scales")
+
 # To prevent copy/paste, expressions are used in the sequel
-plotNames <- c("DDIRatio", "", "PKRatio", "TimeProfile", "ObsVsPred", "ResVsPred", "Histogram", "BoxWhisker", "Tornado")
+plotNames <- c("DDIRatio", "", "TimeProfile", "ObsVsPred", "ResVsPred", "Histogram", "PKRatio", "BoxWhisker", "Tornado")
 
 # Epression is "<plotname>plotConfiguration <- <PlotName>PlotConfiguration$new()"
 createPlotConfigurations <- parse(text = paste0(tolower(plotNames), "plotConfiguration <- ", plotNames, "PlotConfiguration$new()"))
@@ -12,4 +14,32 @@ checkExpandIsFalse <- parse(text = paste0("expect_false(", tolower(plotNames[-1]
 test_that("Default expand is true for DDI ratio only", {
   eval(checkExpandIsFalse)
   expect_true(ddiratioplotConfiguration$defaultExpand)
+})
+
+
+test_that("Default x and y scales is log for ddi ratio", {
+  expect_equal(ddiratioplotConfiguration$defaultXScale, Scaling$log)
+  expect_equal(ddiratioplotConfiguration$defaultYScale, Scaling$log)
+})
+
+test_that("Default x scale is linear and y scale is log for pk ratio", {
+  expect_equal(pkratioplotConfiguration$defaultXScale, Scaling$lin)
+  expect_equal(pkratioplotConfiguration$defaultYScale, Scaling$log)
+})
+
+test_that("Default x scale is linear and y scale is discrete for tornado plot", {
+  expect_equal(tornadoplotConfiguration$defaultXScale, Scaling$lin)
+  expect_equal(tornadoplotConfiguration$defaultYScale, Scaling$discrete)
+})
+
+test_that("Default x scale is discrete and y scale is linear for box-whisker plot", {
+  expect_equal(boxwhiskerplotConfiguration$defaultXScale, Scaling$discrete)
+  expect_equal(boxwhiskerplotConfiguration$defaultYScale, Scaling$lin)
+})
+
+checkXScaleIsLinear <- parse(text = paste0("expect_equal(", tolower(plotNames[2:6]), "plotConfiguration$defaultXScale, Scaling$lin)"))
+checkYScaleIsLinear <- parse(text = paste0("expect_equal(", tolower(plotNames[2:6]), "plotConfiguration$defaultYScale, Scaling$lin)"))
+test_that("Default scales are linear for other plots", {
+  eval(checkXScaleIsLinear)
+  eval(checkYScaleIsLinear)
 })
