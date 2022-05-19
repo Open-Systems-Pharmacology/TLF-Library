e2a663c7528389c0c939da277175df432d7611df
Unknown <chelle.pierre@gmail.com>
2022-05-19 09:45:31 -0400

    Fixes #280 background elements defined a priori
    This allows to switch off grid at creation of plot configuration

--- R/plotconfiguration-background.R
+++ R/plotconfiguration-background.R
@@ -37,8 +37,8 @@ BackgroundConfiguration <- R6::R6Class(
       eval(validateAreaExpression)
       eval(validateLineExpression)
 
-      setAreaExpression <- parse(text = paste0("private$.", areaFieldNames, " <- ", areaFieldNames, " %||% currentTheme$background$", areaFieldNames))
-      setLineExpression <- parse(text = paste0("private$.", lineFieldNames, " <- ", lineFieldNames, " %||% currentTheme$background$", lineFieldNames))
+      setAreaExpression <- parse(text = paste0("self$", areaFieldNames, " <- ", areaFieldNames, " %||% currentTheme$background$", areaFieldNames))
+      setLineExpression <- parse(text = paste0("self$", lineFieldNames, " <- ", lineFieldNames, " %||% currentTheme$background$", lineFieldNames))
       eval(setAreaExpression)
       eval(setLineExpression)
     },
@@ -75,27 +75,51 @@ BackgroundConfiguration <- R6::R6Class(
     },
     #' @field plot `BackgroundElement` object
     plot = function(value) {
-      requestOnElement(private$.plot, value)
+      if (missing(value)) { 
+        return(private$.plot) 
+      } 
+      validateIsOfType(value, "BackgroundElement", nullAllowed = TRUE) 
+      private$.plot <- value %||% private$.plot
     },
     #' @field panel `BackgroundElement` object
     panel = function(value) {
-      requestOnElement(private$.panel, value)
+      if (missing(value)) { 
+        return(private$.panel) 
+      } 
+      validateIsOfType(value, "BackgroundElement", nullAllowed = TRUE) 
+      private$.panel <- value %||% private$.panel
     },
     #' @field xAxis `LineElement` object
     xAxis = function(value) {
-      requestOnElement(private$.xAxis, value)
+      if (missing(value)) { 
+        return(private$.xAxis) 
+      } 
+      validateIsOfType(value, "LineElement", nullAllowed = TRUE) 
+      private$.xAxis <- value %||% private$.xAxis
     },
     #' @field yAxis `LineElement` object
     yAxis = function(value) {
-      requestOnElement(private$.yAxis, value)
+      if (missing(value)) { 
+        return(private$.yAxis) 
+      } 
+      validateIsOfType(value, "LineElement", nullAllowed = TRUE) 
+      private$.yAxis <- value %||% private$.yAxis
     },
     #' @field xGrid `LineElement` object
     xGrid = function(value) {
-      requestOnElement(private$.xGrid, value)
+      if (missing(value)) { 
+        return(private$.xGrid) 
+      } 
+      validateIsOfType(value, "LineElement", nullAllowed = TRUE) 
+      private$.xGrid <- value %||% private$.xGrid
     },
     #' @field yGrid `LineElement` object
     yGrid = function(value) {
-      requestOnElement(private$.yGrid, value)
+      if (missing(value)) { 
+        return(private$.yGrid) 
+      } 
+      validateIsOfType(value, "LineElement", nullAllowed = TRUE) 
+      private$.yGrid <- value %||% private$.yGrid
     }
   ),
   private = list(
@@ -180,23 +204,3 @@ LineElement <- R6::R6Class(
     }
   )
 )
-
-#' @keywords internal
-requestOnElement <- function(field, value) {
-  if (missing(value)) {
-    return(field)
-  }
-  # Update the element partially in case of names list
-  if (isOfType(value, "list")) {
-    for (fieldName in c("color", "size", "linetype")) {
-      field[[fieldName]] <- value[[fieldName]] %||% field[[fieldName]]
-    }
-    if (isOfType(field, "BackgroundElement")) {
-      field[["fill"]] <- value[["fill"]] %||% field[["fill"]]
-    }
-  }
-  # Or update the whole element R6 object is used
-  if (isOfType(value, "BackgroundElement")) {
-    field <- value
-  }
-}
