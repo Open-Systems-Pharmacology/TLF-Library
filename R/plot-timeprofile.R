9640f5407bd2849651371300581a9ffebea57088
Pierre Chelle <45343665+pchelle@users.noreply.github.com>
2022-07-25 11:26:46 -0400

    Fixes #333 set cap width of errorbars (#344)
    * Fixes #333 set cap width of errorbars
    
    Currently, width is a global setting applied the same way to all error bars
    
    * Absolute cap values are now relative and width renamed to extent
    
    Renaming width to extent to prevent user confusion when used for horizontal errorbars whose caps become vertical
    
    * Fixes #333 cap extent renamed cap size and use consistent unit in pts
    
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>

--- R/plot-timeprofile.R
+++ R/plot-timeprofile.R
@@ -150,7 +150,8 @@ plotTimeProfile <- function(data = NULL,
           x = observedMapLabels$x,
           ymin = observedMapLabels$ymin,
           ymax = observedMapLabels$y,
-          color = observedMapLabels$color
+          color = observedMapLabels$color,
+          group = observedMapLabels$color
         ),
         size = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$size, position = 0, aesthetic = "size"),
         linetype = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$linetype, position = 0, aesthetic = "linetype"),
@@ -163,10 +164,37 @@ plotTimeProfile <- function(data = NULL,
           x = observedMapLabels$x,
           ymin = observedMapLabels$y,
           ymax = observedMapLabels$ymax,
-          color = observedMapLabels$color
+          color = observedMapLabels$color,
+          group = observedMapLabels$color
         ),
         size = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$size, position = 0, aesthetic = "size"),
         linetype = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$linetype, position = 0, aesthetic = "linetype"),
+        alpha = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$alpha, position = 0, aesthetic = "alpha"),
+        show.legend = FALSE
+      ) + 
+      ggplot2::geom_point(
+        data = mapObservedData,
+        mapping = ggplot2::aes_string(
+          x = observedMapLabels$x,
+          y = observedMapLabels$ymin,
+          color = observedMapLabels$color,
+          group = observedMapLabels$color
+        ),
+        size = tlfEnv$defaultErrorbarCapSize,
+        shape = "_",
+        alpha = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$alpha, position = 0, aesthetic = "alpha"),
+        show.legend = FALSE
+      ) +
+      ggplot2::geom_point(
+        data = mapObservedData,
+        mapping = ggplot2::aes_string(
+          x = observedMapLabels$x,
+          y = observedMapLabels$ymax,
+          color = observedMapLabels$color,
+          group = observedMapLabels$color
+        ),
+        size = tlfEnv$defaultErrorbarCapSize,
+        shape = "_",
         alpha = .getAestheticValues(n = 1, selectionKey = plotConfiguration$errorbars$alpha, position = 0, aesthetic = "alpha"),
         show.legend = FALSE
       )
