3840ab9a647e8f7ae685835bcfc1ee7426c808c1
Pierre Chelle <45343665+pchelle@users.noreply.github.com>
2022-09-22 12:13:55 -0400

    Fixes #381 Prevent log ticks from crashing plots (#382)

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,28 @@
+#' @title isBetween
+#' @description Assess if `x` is between `left` and `right` bounds.
+#' Shortcut for `x >= left & x <= right` if `strict=FALSE` (default).
+#' Shortcut for `x > left & x < right` if `strict=TRUE`.
+#' @param x Numeric values to assess
+#' @param left Numeric value(s) used as lower bound
+#' @param right Numeric value(s) used as upper bound
+#' @param strict Logical value defining if `x` is strictly between `left` and `right`.
+#' Default value is `FALSE`.
+#' @return Logical values
+#' @export
+#' @examples
+#' isBetween(1:12, 7, 9)
+#'
+#' x <- rnorm(1e2)
+#' x[isBetween(x, -1, 1)]
+#'
+#' isBetween(x, cos(x) + 1, cos(x) - 1)
+isBetween <- function(x, left, right, strict = FALSE) {
+  if (strict) {
+    return(x > left & x < right)
+  }
+  return(x >= left & x <= right)
+}
+
 #' @title getSymmetricLimits
 #' @description Get symmetric limits from a set of values
 #' @param values numeric values
