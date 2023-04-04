f19a74f6d6a2bd7dd85f080f7e44204245f622bf
Felix MIL <felix.mil@esqlabs.com>
2023-04-04 11:04:10 +0200

    Add unit test for timeprofile plots

--- tests/testthat/test-plot-timeprofile.R
+++ tests/testthat/test-plot-timeprofile.R
@@ -1,22 +1,61 @@
 obsData <- data.frame(name = c("dataset1", "dataset1", "dataset2", "dataset2", "dataset2"),
-                      x = c(1, 2, 1, 2, 3), y = c(5, 0.2, 2, 3, 4))
+                      x = c(1, 2, 1, 2, 3), y = c(5, 0.2, 2, 1, 3),
+                      group = c("group A", "group A","group A", "group B", "group B"),
+                      lloq = c(rep(0.5, 3), rep(1.5,2))
+)
 
 simTime <- seq(1, 10, 0.1)
 
 simData <- data.frame(
- x = simTime,
- y = 10 * exp(-simTime),
- ymin = 8 * exp(-simTime),
- ymax = 12 * exp(-simTime)
+  x = simTime,
+  y = 10 * exp(-simTime),
+  ymin = 8 * exp(-simTime),
+  ymax = 12 * exp(-simTime)
 )
 
-color <- fill <- "group"
-linetype <- shape <- "name"
 
-plotTimeProfile(
- # data = simData,
- observedData = obsData,
- # dataMapping = TimeProfileDataMapping$new(x = "x", y = "y",  ymin = "ymin", ymax = "ymax"),
- observedDataMapping = ObservedDataMapping$new(x = "x", y = "y", shape = shape, color = shape)
-)
+
+
+test_that("plotTimeProfile works ", {
+
+  skip_if_not_installed("vdiffr")
+  skip_if(getRversion() < "4.1")
+
+  color <- fill <- "group"
+  linetype <- shape <- "name"
+
+  vdiffr::expect_doppelganger(
+    title = "basic",
+    fig = plotTimeProfile(
+      data = simData,
+      observedData = obsData,
+      dataMapping = TimeProfileDataMapping$new(x = "x", y = "y",  ymin = "ymin", ymax = "ymax"),
+      observedDataMapping = ObservedDataMapping$new(x = "x", y = "y", group = "group", shape = shape, color = shape)
+    )
+  )
+
+
+  vdiffr::expect_doppelganger(
+    title = "with lloq",
+    fig = plotTimeProfile(
+      data = simData,
+      observedData = obsData,
+      dataMapping = TimeProfileDataMapping$new(x = "x", y = "y",  ymin = "ymin", ymax = "ymax"),
+      observedDataMapping = ObservedDataMapping$new(x = "x", y = "y", group = "group", shape = shape, color = shape,lloq = "lloq")
+    )
+  )
+
+
+  vdiffr::expect_doppelganger(
+    title = "with lloq and plotConfiguration",
+    fig = plotTimeProfile(
+      data = simData,
+      observedData = obsData,
+      dataMapping = TimeProfileDataMapping$new(x = "x", y = "y",  ymin = "ymin", ymax = "ymax"),
+      observedDataMapping = ObservedDataMapping$new(x = "x", y = "y", group = "group", shape = shape, color = shape,lloq = "lloq"),
+      plotConfiguration = TimeProfilePlotConfiguration$new(lloqDirection = "horizontal")
+    )
+  )
+
+})
 
