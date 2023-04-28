54aef977d673c60f457e4e1f8307c6aac443167a
Felix MIL <felix.mil@esqlabs.com>
2023-04-28 10:35:59 +0200

    Suppress Notes during CHECK about not found variables (special ggplot2 variables)

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,6 @@ tlfEnv$currentTheme <- Theme$new()
 
 # Default theme is minimal when package is loaded
 useMinimalTheme()
+
+# Quiets concerns of R CMD check for ggplot2
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("count","ncount","width"))
