5a0eee8c9852de866ac8d48c0ce8d332c13968f3
Felix MIL <felix.mil@esqlabs.com>
2023-08-07 09:45:53 +0200

    Move Label format test to more appropriate location + add test that verify if Markdown/HTML syntax in Label is working in ggtext + Update snaps

--- tests/testthat/test-font-label.R
+++ tests/testthat/test-font-label.R
@@ -50,3 +50,57 @@ test_that("Non-empty label is translated as element_text and element_textbox", {
     "element_textbox"
   )
 })
+
+test_that("Long texts are properly handled in labels",{
+
+
+  vdiffr::expect_doppelganger(
+    "very long labels",
+    fig = initializePlot(plotConfiguration = PlotConfiguration$new(
+      title = paste("Title: This is a", paste(rep("very", 40), collapse = " "), "long title"),
+      subtitle = paste("Subtitle: This is a", paste(rep("very", 40), collapse = " "), "long subtitle"),
+      xlabel = paste("xlabel: This is a", paste(rep("very", 40), collapse = " "), "long x label"),
+      ylabel =  paste("ylabel: This is a", paste(rep("very", 40), collapse = " "), "long y label"),
+      caption =  paste("Caption: This is a", paste(rep("very", 40), collapse = " "), "long caption"))
+    )
+  )
+
+  vdiffr::expect_doppelganger(
+    "very long flipped labels",
+    fig = initializePlot(plotConfiguration = PlotConfiguration$new(
+      title = paste("Title: This is a", paste(rep("very", 40), collapse = " "), "long title"),
+      subtitle = paste("Subtitle: This is a", paste(rep("very", 40), collapse = " "), "long subtitle"),
+      xlabel =Label$new(text = paste("ylabel: This is a", paste(rep("very", 40), collapse = " "), "long x label"), angle = 90),
+      ylabel =  Label$new(text = paste("ylabel: This is a", paste(rep("very", 40), collapse = " "), "long y label"), angle = 0),
+      caption =  paste("Caption: This is a", paste(rep("very", 40), collapse = " "), "long caption"),
+      watermark = Label$new(text = "watermark", angle = 45))
+    )
+  )
+
+  expect_warning(
+    initializePlot(plotConfiguration = PlotConfiguration$new(
+      title = paste("Title: This is a", paste(rep("very", 40), collapse = " "), "long title"),
+      subtitle = paste("Subtitle: This is a", paste(rep("very", 40), collapse = " "), "long subtitle"),
+      xlabel =Label$new(text = paste("ylabel: This is a", paste(rep("very", 40), collapse = " "), "long x label"), angle = 75),
+      ylabel =  Label$new(text = paste("ylabel: This is a", paste(rep("very", 40), collapse = " "), "long y label"), angle = 30),
+      caption =  paste("Caption: This is a", paste(rep("very", 40), collapse = " "), "long caption"),
+      watermark = Label$new(text = "watermark", angle = 45))
+    )
+  )
+
+})
+
+test_that("Markdown syntax is not supported because Label and Font overwrite its with the FontFace argument",{
+  # This test should break when the issue is fixed.
+  vdiffr::expect_doppelganger(
+    "Plot with Markdown",
+    fig = initializePlot(
+    plotConfiguration = PlotConfiguration$new(
+      title = "**This title should be bold**",
+      subtitle = "*This subtitle should be italic*",
+      caption = "This caption has style<br>
+<span style = 'font-size:10pt;'>because it uses <span style = 'color:#0072B2;'><b>HTML</b></span> instead of
+<span style = 'color:#D55E00;'><i>Markdown syntax</i></span></span>")
+  )
+  )
+})
